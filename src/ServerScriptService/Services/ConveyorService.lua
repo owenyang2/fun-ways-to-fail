0f1cbb05f15700ba41d7884471ad375e7c942c40
LightDotz <82738322+LightDotz@users.noreply.github.com>
2022-09-08 19:30:57 -0400

    make conveyor animation

--- src/ServerScriptService/Services/ConveyorService.lua
+++ src/ServerScriptService/Services/ConveyorService.lua
@@ -1,11 +1,42 @@
 local RepStorage = game:GetService("ReplicatedStorage")
+local TweenService = game:GetService("TweenService")
 
 local Knit = require(RepStorage.Packages.Knit)
+local TableUtil = require(RepStorage.Packages.TableUtil)
 
 local ConveyorService = Knit.CreateService {
     Name = "ConveyorService",
     --Client = {} -- you probably dont want client accessing any functions
 }
+
+function ConveyorService:CreateAnimations()
+    local info = TweenInfo.new(self.AnimationTime, Enum.EasingStyle.Linear, Enum.EasingDirection.Out)
+
+    for conveyor, _ in pairs(self.Conveyors) do
+        local tween = TweenService:Create(conveyor.Texture, info, {OffsetStudsV = 6})
+        
+        self.Animations[conveyor] = tween
+    
+        tween.Completed:Connect(function(playbackState)
+            if playbackState == Enum.PlaybackState.Completed then
+                conveyor.Texture.OffsetStudsV = 0
+                tween:Play()
+            end
+        end)
+    end
+end
+
+function ConveyorService:PlayAnimation()
+    for conveyor, tween in pairs(self.Animations) do
+        tween:Play()
+    end
+end
+
+function ConveyorService:CancelAnimation()
+    for conveyor, tween in pairs(self.Animations) do
+        tween:Cancel()
+    end
+end
 
 function ConveyorService:EnableConveyors()
     for conveyor, info in pairs(self.Conveyors) do
@@ -13,12 +44,16 @@ function ConveyorService:EnableConveyors()
         
         conveyor.AssemblyLinearVelocity = info.Direction * speed
     end
+
+    self:PlayAnimation()
 end
 
 function ConveyorService:DisableConveyors()
     for conveyor, info in pairs(self.Conveyors) do     
         conveyor.AssemblyLinearVelocity = 0
     end
+
+    self:CancelAnimation()
 end
 
 function ConveyorService:KnitStart()
@@ -32,7 +67,11 @@ function ConveyorService:KnitStart()
     }
 
     self.DefaultConveyorSpeed = 100
+    self.AnimationTime = 0.2
 
+    self.Animations = {}
+
+    self:CreateAnimations()
     self:EnableConveyors()
 end
 
