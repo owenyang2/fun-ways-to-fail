4de35303517031c9c2c7dc659c7d008a574fa3c6
LightDotz <82738322+LightDotz@users.noreply.github.com>
2023-09-22 22:00:22 -0400

    improve rocket

--- src/ServerScriptService/Services/MachineService/Rocket.lua
+++ src/ServerScriptService/Services/MachineService/Rocket.lua
@@ -15,6 +15,11 @@ Rocket.AvailableInstances = {
     game.Workspace.PlaceModels:FindFirstChild("Rocket")
 }
 
+function Rocket:SetNight()
+    local tween = TweenService:Create(game.Lighting, self.Config.FlyTimeInfo, {ClockTime = self.Config.EndClockTime})
+    tween:Play()
+end
+
 function Rocket:Start()
     self.Instance.Enter.Touched:Connect(function(part)
         local chr = part.Parent
@@ -22,13 +27,13 @@ function Rocket:Start()
 
         if plr then
             local realRocket = self.Instance.Rocket
-
-            local fakeRocket = realRocket:Clone()
-            fakeRocket.Name = "FakeRocket"
-            fakeRocket.Parent = self.Instance
-
             realRocket.Transparency = 1
             realRocket.CanCollide = false
+
+            local fakeRocket = realRocket:Clone() -- clone after make actual rocket disappear fixes camera clipping
+            fakeRocket.Transparency = 0 -- because of above, reset transparency
+            fakeRocket.Name = "FakeRocket"
+            fakeRocket.Parent = self.Instance
 
             chr:SetPrimaryPartCFrame(fakeRocket.CFrame)
 
@@ -37,7 +42,7 @@ function Rocket:Start()
             chrWeld.Part1 = fakeRocket
             chrWeld.Parent = fakeRocket
 
-            task.wait(self.DelayTime)
+            task.wait(self.Config.LaunchDelay)
 
             fakeRocket.Anchored = false
             fakeRocket.CanCollide = false
@@ -47,40 +52,50 @@ function Rocket:Start()
             local launchVelo = Instance.new("LinearVelocity")
             launchVelo.Attachment0 = att
             launchVelo.MaxForce = math.huge
-            launchVelo.VectorVelocity = self.LaunchVelo
+            launchVelo.VectorVelocity = self.Config.LaunchVelo
             launchVelo.Parent = fakeRocket
 
             chr.Humanoid.WalkSpeed = 0
             chr.Humanoid.JumpHeight = 0
 
-            task.wait(self.FlyTime)
+            self:SetNight()
+            task.wait(self.Config.FlyTime)
 
             fakeRocket.Anchored = true
             launchVelo:Destroy()
+
+            task.wait(self.Config.DropDelay)
+
             chrWeld:Destroy()
 
             local plrNoGrav = Instance.new("LinearVelocity")
             plrNoGrav.Attachment0 = chr.HumanoidRootPart.RootRigAttachment
             plrNoGrav.MaxForce = math.huge
-            plrNoGrav.VectorVelocity = self.GravVelo
+            plrNoGrav.VectorVelocity = self.Config.GravVelo
             plrNoGrav.Parent = chr
 
             chr.Humanoid.WalkSpeed = game.StarterPlayer.CharacterWalkSpeed
             chr.Humanoid.JumpHeight = game.StarterPlayer.CharacterJumpHeight
 
             for _, bodyPart in ipairs(self.BodyColors) do
-                local tween = TweenService:Create(chr["Body Colors"], self.TurnWhiteInfo, {[bodyPart] = self.WhiteColor})
+                local tween = TweenService:Create(chr["Body Colors"], self.Config.TurnWhiteInfo, {[bodyPart] = self.Config.WhiteColor})
                 tween:Play()
             end
 
-            local kTween = TweenService:create(chr.Humanoid, self.KillInfo, {Health = 0})
+            local kTween = TweenService:create(chr.Humanoid, self.Config.KillInfo, {Health = 0})
             kTween:Play()
 
-            task.wait(self.KillInfo.Time + 3)
+            -- reset time when player respawns
+            local tempConnect
+            tempConnect = plr.CharacterAdded:Connect(function()
+                tempConnect:Disconnect()
+                game.Lighting.ClockTime = self.Config.DefaultClockTime
+            end)
+
+            task.wait(self.Config.KillInfo.Time + game.Players.RespawnTime)
             fakeRocket:Destroy()
 
             -- reset
-
             realRocket.CanCollide = true
             realRocket.Transparency = 0
         end
@@ -94,12 +109,6 @@ function Rocket.new(baseTbl)
     local self = setmetatable(TableUtil.Assign(baseTbl, {
         Instance = newInst,
 
-        LaunchVelo = Vector3.new(0, 75, 0),
-        GravVelo = Vector3.new(0, -15, 0),
-
-        DelayTime = 3,
-        FlyTime = 7,
-        
         BodyColors = {
             "HeadColor3",
             "LeftArmColor3",
@@ -109,15 +118,29 @@ function Rocket.new(baseTbl)
             "TorsoColor3"
         },
 
-        TurnWhiteInfo = TweenInfo.new(5, Enum.EasingStyle.Sine, Enum.EasingDirection.In),
-        KillInfo = TweenInfo.new(7, Enum.EasingStyle.Sine, Enum.EasingDirection.In),
-
-        WhiteColor = Color3.fromRGB(255, 255, 255),
-
-        CameraZoom = 50,
+        Config = {
+            TurnWhiteInfo = TweenInfo.new(5, Enum.EasingStyle.Sine, Enum.EasingDirection.In),
+            KillInfo = TweenInfo.new(7, Enum.EasingStyle.Sine, Enum.EasingDirection.In),
+    
+            WhiteColor = Color3.fromRGB(255, 255, 255),
+    
+            CameraZoom = 50,
+    
+            DefaultClockTime = game.Lighting.ClockTime, -- default should be 14.5 unless it's manually changed
+            EndClockTime = 24,
+    
+            LaunchVelo = Vector3.new(0, 75, 0),
+            GravVelo = Vector3.new(0, -15, 0),
+    
+            LaunchDelay = 3,
+            FlyTime = 7,
+            DropDelay = 1.5,
+        },
 
         _trove = Trove.new()
     }), Rocket)
+
+    self.Config.FlyTimeInfo = TweenInfo.new(self.Config.FlyTime - 1, Enum.EasingStyle.Linear, Enum.EasingDirection.In)
 
     return self
 end
